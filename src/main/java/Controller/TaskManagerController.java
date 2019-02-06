32488d99d2b2ecc8e8445768d8179ca6ab730406
Yaroslav Lichnyi <44211382+YaroslavLichnyi@users.noreply.github.com>
2019-02-06 23:48:54 +0200

    Work with files

--- src/main/java/Controller/TaskManagerController.java
+++ src/main/java/Controller/TaskManagerController.java
@@ -1,9 +1,12 @@
 package Controller;
+import Model.TaskIO;
 import org.apache.log4j.Logger;
 import Model.ArrayTaskList;
 import Model.Task;
 import View.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Observable;
@@ -12,9 +15,12 @@ public class TaskManagerController extends Observable {
     ArrayTaskList tasks ;
     TaskManagerView view ;
     private static final Logger log = Logger.getLogger(TaskManagerController.class);
+    File tasksTextFile = new File("tasksForTaskManager.txt");
     public TaskManagerController() {
 
         tasks = new ArrayTaskList();
+        readTasks();
+/*
         SimpleDateFormat taskFormat = new SimpleDateFormat("ddMMyyyyHHmmss");
         String some = "03022019220000";
         String some1 = "23022019220000";
@@ -33,6 +39,7 @@ public class TaskManagerController extends Observable {
         }
         tasks.add(new Task("Football", new Date(1000000000), new Date(500000000), 100000));
         tasks.add(new Task("Coding", new Date(1000000000),new Date(400000000), 500000) );
+        */
         view = new TaskManagerView(tasks, this);
         this.addObserver(view);
         TaskTableFrame taskTableFrame = new TaskTableFrame(tasks, this);
@@ -44,6 +51,7 @@ public class TaskManagerController extends Observable {
         tasks.add(task);
         this.notifyObservers(tasks);
         view.update(this, tasks);
+
     }
 
     public void deleteTask(Task task){
@@ -54,6 +62,21 @@ public class TaskManagerController extends Observable {
     public void editTask(Task oldTask, Task newTask ){
         this.deleteTask(oldTask);
         this.addTask(newTask);
+    }
+    public void writeTasks(){
+        try {
+            TaskIO.writeText(tasks, tasksTextFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void readTasks(){
+        try {
+            TaskIO.readText(tasks, tasksTextFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
