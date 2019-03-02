669b70976d42bcc4e40894d32bf740fbf07922d7
Yaroslav Lichnyi <44211382+YaroslavLichnyi@users.noreply.github.com>
2019-03-03 00:14:38 +0200

    Adding new classes

---
+++ src/main/java/View/TaskManagerFillingFormGui.java
@@ -0,0 +1,59 @@
+package View;
+
+import Model.TaskInfo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+public abstract class TaskManagerFillingFormGui extends TaskManagerGUI{
+    protected JComboBox cmbDaysFrom;
+    protected JComboBox cmbMonthesFrom;
+    protected JComboBox cmbYearsFrom;
+    protected JComboBox cmbSecondsFrom;
+    protected JComboBox cmbMinutesFrom;
+    protected JComboBox cmbHoursFrom;
+    protected JComboBox cmbDaysTo;
+    protected JComboBox cmbMonthesTo;
+    protected JComboBox cmbYearsTo;
+    protected JComboBox cmbSecondsTo;
+    protected JComboBox cmbMinutesTo;
+    protected JComboBox cmbHoursTo;
+
+    public TaskManagerFillingFormGui() throws HeadlessException {
+        super();
+    }
+
+    protected Date getInputedStartTime(){
+        String seconds =    (String) cmbSecondsFrom.getSelectedItem();
+        String minutes =    (String) cmbMinutesFrom.getSelectedItem();
+        String hours   =    (String) cmbHoursFrom.getSelectedItem();
+        String days    =    (String) cmbDaysFrom.getSelectedItem();
+        String monthes =    (String) cmbMonthesFrom.getSelectedItem();
+        String years   =    (String) cmbYearsFrom.getSelectedItem();
+        String strDateFrom = days + monthes + years + hours + minutes + seconds;
+        return TaskInfo.createDate(strDateFrom);
+    }
+
+    protected  Date getInputedEndTime(){
+        String seconds =    (String) cmbSecondsTo.getSelectedItem();
+        String minutes =    (String) cmbMinutesTo.getSelectedItem();
+        String hours   =    (String) cmbHoursTo.getSelectedItem();
+        String days    =    (String) cmbDaysTo.getSelectedItem();
+        String monthes =    (String) cmbMonthesTo.getSelectedItem();
+        String years   =    (String) cmbYearsTo.getSelectedItem();
+        String strDateTo = days + monthes + years + hours + minutes + seconds;
+        return TaskInfo.createDate(strDateTo);
+    }
+
+    protected boolean isIncorrectTimeInputed(){
+        String daysFrom  =  (String) cmbDaysFrom.getSelectedItem();
+        String monthesFrom =(String) cmbMonthesFrom.getSelectedItem();
+        String yearsFrom =  (String) cmbYearsFrom.getSelectedItem();
+        String daysTo    =  (String) cmbDaysTo.getSelectedItem();
+        String monthesTo =  (String) cmbMonthesTo.getSelectedItem();
+        String yearsTo   =  (String) cmbYearsTo.getSelectedItem();
+        return TaskInfo.isDateIncorrect(Integer.parseInt(daysFrom),Integer.parseInt(monthesFrom),Integer.parseInt(yearsFrom))
+                || TaskInfo.isDateIncorrect(Integer.parseInt(daysTo),Integer.parseInt(monthesTo),Integer.parseInt(yearsTo));
+    }
+}
---
+++ src/main/java/View/TaskManagerGUI.java
@@ -0,0 +1,40 @@
+package View;
+
+import Controller.TaskManagerController;
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class TaskManagerGUI extends JFrame {
+    protected TaskManagerController controller;
+    protected JPanel panel;
+    protected GridBagLayout gridBagLayout ;
+    protected GridBagConstraints gridBag ;
+    protected Toolkit toolkit;
+    protected Dimension dimension;
+
+    public TaskManagerGUI() throws HeadlessException {
+        super("Task Manager");
+        toolkit = Toolkit.getDefaultToolkit();
+        dimension = toolkit.getScreenSize();
+        panel = new JPanel();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        gridBag = new GridBagConstraints();
+        gridBagLayout = new GridBagLayout();
+
+    }
+
+    public void setController(TaskManagerController controller) {
+        this.controller = controller;
+    }
+
+    public TaskManagerController getController() {
+        return controller;
+    }
+
+    protected abstract void addElements();
+}
